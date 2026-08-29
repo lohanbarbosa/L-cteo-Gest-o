d246384a376073c9a8473542b04fd77c5f851085
lohanbarbosa <lohan.sanchezs@gmail.com>
2026-08-29 00:55:14 -0300

    Update _globalContextProviders.tsx

--- components/_globalContextProviders.tsx
+++ components/_globalContextProviders.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "../helpers/useAuth";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export const GlobalContextProviders: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>{children}</AuthProvider>
+  </QueryClientProvider>
+);
